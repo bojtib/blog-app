788d6bcd167573d674a93a542b180381000022a5
Bence Boejti <bojti@amazon.com>
2023-05-25 16:10:50 +0200

    Third step adding link.

--- src/BlogPost.tsx
+++ src/BlogPost.tsx
@@ -18,6 +18,10 @@ function BlogPost(props: Props) {
         <Card.Body>
             <Card.Title>{post.title}</Card.Title>
             <Card.Text>{post.text}</Card.Text>
+            {
+                // This line make sure that the link is only displayed if it exists.
+                post.link && <Card.Link href={post.link.url}>{post.link.title}</Card.Link>
+            }
         </Card.Body>
         <Card.Footer>
             <small className="text-muted"> Author: {post.author}</small>
